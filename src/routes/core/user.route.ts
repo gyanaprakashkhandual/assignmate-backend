e350e397df696a6840b1d9ac0cb904bc82a6aa92
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-05-04 20:33:41 +0530

    Cookit issued fixed trying for the server

--- src/routes/core/user.route.ts
+++ src/routes/core/user.route.ts
@@ -53,7 +53,7 @@ router.post("/logout", requireAuth, (req, res) => {
     res.clearCookie("token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        sameSite: "none",
     });
     res.status(200).json({ message: "Logged out successfully" });
 });
